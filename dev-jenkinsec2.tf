48c07a59535d4eb503aeb5712eb35874121fe3c2
D DraFz <61958249+ddrafz-mbl@users.noreply.github.com>
2021-05-18 20:33:57 +0700

    Create dev-jenkinsec2.tf

---
+++ dev-jenkinsec2.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  region  = var.region
+}
+
+resource "aws_security_group" "Jenkinsec2" {
+  name = "Jenkins EC2"
+
+  ingress {
+    from_port   = 8080
+    protocol    = "tcp"    
+    to_port     = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "Jenkins-EC2" {
+  instance_type          = var.instance_type
+  ami                    = data.aws_ami.ubuntu.id
+  vpc_security_group_ids = [aws_security_group.Jenkinsec2.id]
+  key_name               = "linux-key"
+
+  tags = {
+    Name = "terraform-jenkins-master"
+  }
+  user_data = file("userdata.sh")
+
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
+
+}
+
+output "jenkins_endpoint" {
+  value = formatlist("http://%s:%s/", aws_instance.Jenkins-EC2.*.public_ip, "8080")
+}
